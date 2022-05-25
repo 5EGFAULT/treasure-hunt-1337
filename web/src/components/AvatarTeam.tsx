2697fb93f05a6046f25e5d76c3fd68f0dfe71e43
Souinia Ayoub <souiniaayoub@gmail.com>
2022-05-25 01:14:39 +0100

    leader board done

--- web/src/components/AvatarTeam.tsx
+++ web/src/components/AvatarTeam.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React from "react";
+import gold from "../../assets/gold.svg";
+import silver from "../../assets/silver.svg";
+import bronze from "../../assets/bronze.svg";
+type Props = {
+  type: 1 | 2 | 3;
+};
+const defurl = "https://wallpaperaccess.com/full/532051.jpg";
 
-type Props = {}
-
- export default function 
-({}: Props) {
+export default function ({ type }: Props) {
   return (
-	<div className='  '>
-		
-	</div>
-  )
+    <div
+      className={
+        (type == 1 ? " w-[150px] h-[150px]  " : " w-[120px] h-[120px]  ") +
+        " relative "
+      }
+    >
+      <img
+        src={defurl}
+        alt="team image"
+        className="inline object-cover rounded-full h-full border-4 border-[#FFFFFF] drop-shadow-lg"
+      />
+      <img
+        src={type == 1 ? gold : type == 2 ? silver : bronze}
+        alt=" star "
+        className={
+          "absolute -bottom-0 left-1/2   translate-y-1/2	-translate-x-1/2  bg-no-repeat bg-center" +
+          (type == 1
+            ? "h-[64px] w-[64px]"
+            : type == 2
+            ? "h-[38px] w-[38px]"
+            : "h-[30px] w-[30px]")
+        }
+      />
+    </div>
+  );
 }
--- web/src/pages/Leaderboard.tsx
+++ web/src/pages/Leaderboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AvatarTeam from "../components/AvatarTeam";
 import TeamTile from "./TeamTile";
 
 type Props = {};
@@ -6,12 +7,26 @@ type Props = {};
 export default function Leaderboard({}: Props) {
   return (
     <div className="w-[100%] relative  h-full  bg-[#F4F4F4] flex flex-col  items-center overflow-y-scroll">
-      <div className="bg-[#017DE9] min-h-[180px] w-full">
-		  
-	  </div>
+      <div className="bg-[#017DE9] min-h-[180px] w-full"></div>
       <div className="bg-[linear-gradient(180deg,#017DE9_0%,#F4F4F4_100%)] min-h-[190px] w-full"></div>
       <div className="absolute top-0 w-full h-full p-5">
-        <div className="min-h-[300px]  w-full "></div>
+        <div className="relative min-h-[300px] w-full flex justify-evenly items-center">
+          <div className="absolute z-10  text-center">
+            <div className="mb-5 font-bold text-3xl text-white ">1</div>
+            <AvatarTeam type={1} />
+            <div className="mt-8  font-semibold text-xl text-white ">100xp</div>
+          </div>
+          <div className="absolute mt-4 -translate-x-[75%] text-center">
+            <div className="mb-5 font-bold text-lg text-white ">3</div>
+            <AvatarTeam type={3} />
+            <div className="mt-5 font-semibold text-lg text-white ">100xp</div>
+          </div>
+          <div className=" absolute mt-4 translate-x-[75%] text-center">
+            <div className="mb-5 font-bold text-xl text-white ">2</div>
+            <AvatarTeam type={2} />
+            <div className="mt-5 font-semibold text-lg text-white ">100xp</div>
+          </div>
+        </div>
         <ul className="w-full">
           <TeamTile />
           <TeamTile />
