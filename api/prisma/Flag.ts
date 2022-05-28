c9f6ce590d6ba0c7569e70a52205c4b3f5b36580
Ubuntu <ubuntu@ip-172-31-45-9.eu-west-3.compute.internal>
2022-05-28 02:37:10 +0000

    deploy checks 1..

--- api/prisma/Flag.ts
+++ api/prisma/Flag.ts
@@ -1,14 +1,14 @@
 import { PrismaClient, Prisma } from '@prisma/client'
 export const Flags = [
-	{
-		max_collectors: 3,
-		bounty: 100,
-		name: 'flag 1',
-		secret: 'secret 1',
-		hint: 'hint 1',
-		is_expired: false,
-		is_road: false,
-		place: "place 1",
-	}
+	// {
+	// 	max_collectors: 3,
+	// 	bounty: 100,
+	// 	name: 'flag 1',
+	// 	secret: 'secret 1',
+	// 	hint: 'hint 1',
+	// 	is_expired: false,
+	// 	is_road: false,
+	// 	place: "place 1",
+	// }
 ] as Prisma.FlagCreateManyInput[]
 
--- api/prisma/Team.ts
+++ api/prisma/Team.ts
@@ -1,43 +1,43 @@
 import { PrismaClient, Prisma } from '@prisma/client'
 export const Team = [
-	{
-		id: 1,
-		name: 'team 0',
-		picture: 'fileimgds',
-		password: '123456123',
-	},
-	{
-		id: 2,
-		name: 'team 1',
-		picture: 'fileimg',
-		password: '123456',
-	}
+	// {
+	// 	id: 1,
+	// 	name: 'team 0',
+	// 	picture: 'fileimgds',
+	// 	password: '123456123',
+	// },
+	// {
+	// 	id: 2,
+	// 	name: 'team 1',
+	// 	picture: 'fileimg',
+	// 	password: '123456',
+	// }
 ] as Prisma.TeamCreateManyInput[]
 
 export const Users = [
-	{
-		id: 1,
-		firstName: 'firstName 1',
-		lastName: 'lastName 1',
-		teamId: 1,
-	},
-	{
-		id: 2,
-		firstName: 'firstName 2',
-		lastName: 'lastName 2',
-		teamId: 1,
-	},
-	{
-		id: 3,
-		firstName: 'firstName 3',
-		lastName: 'lastName 3',
-		teamId: 1,
-	},
-	{
-		id: 4,
-		firstName: 'firstName 4',
-		lastName: 'lastName 4',
-		teamId: 1,
-	}
+	// {
+	// 	id: 1,
+	// 	firstName: 'firstName 1',
+	// 	lastName: 'lastName 1',
+	// 	teamId: 1,
+	// },
+	// {
+	// 	id: 2,
+	// 	firstName: 'firstName 2',
+	// 	lastName: 'lastName 2',
+	// 	teamId: 1,
+	// },
+	// {
+	// 	id: 3,
+	// 	firstName: 'firstName 3',
+	// 	lastName: 'lastName 3',
+	// 	teamId: 1,
+	// },
+	// {
+	// 	id: 4,
+	// 	firstName: 'firstName 4',
+	// 	lastName: 'lastName 4',
+	// 	teamId: 1,
+	// }
 
 ] as Prisma.UserCreateManyInput[]
